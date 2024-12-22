3ba0c710dd4fe45f532aa679e49b1f6dd5644bf1
theGuardianCode <nickrichard878@gmail.com>
2024-12-22 13:22:46 +1000

    added circle function

--- main.c
+++ main.c
@@ -11,6 +11,31 @@ typedef struct circle
     int radius;
 } Circle;
 
+void DrawCircle(SDL_Surface* surface, Circle circle, int colour)
+{
+    int low_x = circle.x - circle.radius;
+    int high_x = circle.x + circle.radius;
+    int low_y = circle.y - circle.radius;
+    int high_y = circle.y + circle.radius;
+
+    int radius_squared = circle.radius * circle.radius;
+
+    for (int y = low_y; y <= high_y; y++)
+    {
+        for (int x = low_x; x <= high_x; x++)
+        {
+            // dst^2 = (x^2 + y^2)
+            // if dst2 <= rad2 then draw point
+            int distance_squared = (x-circle.x)*(x-circle.x) + (y-circle.y)*(y-circle.y);
+            if (distance_squared <= radius_squared)
+            {
+                SDL_Rect rect = {x, y, 1, 1};
+                SDL_FillRect(surface, &rect, colour);
+            }
+        }
+    }
+}
+
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -29,9 +54,9 @@ int main()
                 running = 0;
             }
         }
-
-        SDL_Rect rect = (SDL_Rect){0, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
-        SDL_FillRect(surface, &rect, 0xffffffff);
+        
+        Circle circle = {300, 300, 50};
+        DrawCircle(surface, circle, 0xffffffff);
 
         SDL_UpdateWindowSurface(window);
     }
